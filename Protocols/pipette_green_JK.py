8d67789ca179a5d95bf1cf4521cc6cf63bcf8411
James-Kitson <James.Kitson@ncl.ac.uk>
2019-06-12 13:01:57 +0100

    trying to set up a mixing step

--- Protocols/pipette_green_JK.py
+++ Protocols/pipette_green_JK.py
@@ -8,7 +8,7 @@ metadata = {
 
 trough = labware.load('starlab-E2896-0220', '2')
 
-liquid_trash = trough.cols('12')
+liquid_trash = trough.wells('A12')
 
 plate = labware.load('starlab-E2896-0600', '3')
 
@@ -17,24 +17,25 @@ tiprack = [labware.load('opentrons-tiprack-300ul', slot)
 
 pipette=instruments.P300_Multi(mount='left')
 
-pipette.pick_up_tip(tiprack.cols('1'))
+pipette.pick_up_tip(tiprack.wells('A1'))
 
-pipette.distribute(50, trough.cols('1'),
-	plate.cols('1',to='12'),
-	disposal_vol=50,
+pipette.distribute(50, trough.wells('A1'),
+	plate.rows('A'),
+	disposal_vol=30,
 	new_tip='never',
-	touch_tip=True)
+	touch_tip=False)
 pipette.drop_tip()
 
-pipette.pick_up_tip(tiprack.cols('2'))
+pipette.pick_up_tip(tiprack.wells('A2'))
 
-pipette.distribute(50, trough.cols('2'),
-	plate.cols('1','2','3','4','5','6','7','8','9','10','11','12'),
-	disposal_vol=50,
+pipette.distribute(50, trough.wells('A2'),
+	plate.rows('A'),
+	disposal_vol=30,
 	new_tip='never',
-	touch_tip=True)
+	touch_tip=False)
 pipette.drop_tip()
 
-pipette.pick_up_tip(tiprack.cols('3'))
-pipette.mix(3, 50, plate.cols('1','2','3','4','5','6','7','8','9','10','11','12'))
+pipette.pick_up_tip(tiprack.wells('A3'))
+pipette.mix(3, 50, plate.rows('A'), 
+            new_tip='never')
 pipette.drop_tip()
