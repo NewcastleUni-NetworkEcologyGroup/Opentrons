0bcd0f3bc6c0276a2951b57b2e423d3b4b8e60b6
James-Kitson <jameskitson10@gmail.com>
2019-06-19 16:40:34 +0100

    this is a script to try and do multiplate distribution

---
+++ Protocols/Labware_checking_protocols/oil_pipetting_test.py
@@ -0,0 +1,38 @@
+from opentrons import labware, instruments
+
+metadata = {
+    'protocolName': 'Test tips',
+    'author': 'James Kitson <James.Kitson@ncl.ac.uk>',
+    'description': 'A simple test protocol that checks I have set up a give tiprack correctly'
+    }
+
+# Set plates to use
+trough = labware.load('starlab-E2310-1200', '1')
+forward_primer = labware.load('starlab-E1403-0100','10')
+PCR1 = labware.load('starlab-E1403-5200', '4')
+PCR2 = labware.load('starlab-E1403-5200', '5')
+
+# set tips to use
+tips10 = labware.load('tiprack-starlab-S1181-3810', '2')
+tips300 = labware.load('opentrons-tiprack-300ul', '3')
+
+# set pipettes
+pipette10 = instruments.P10_Multi(mount='right')
+pipette300 = instruments.P300_Multi(mount='left')
+
+master_mix = trough.wells('A3')
+
+dest_plates = [PCR1,PCR2]
+
+all_dests = [well for plate in dest_plates for well in plate.rows('A')]
+
+for d in all_dests:
+   # pipette300.pick_up_tip(tiprack_300['A'])
+    pipette300.transfer(30, master_mix, d.top(), blow_out=True, new_tip='never')
+pipette300.drop_tip()
+
+    # forward primer distribution
+for ind, primer in enumerate(forward_primer.rows('A')):
+    dests = [plate.rows('A')[ind] for plate in dest_plates]
+    #pipette10.pick_up_tip(tiprack_10['A'])
+    pipette10.distribute(1, primer, dests)
