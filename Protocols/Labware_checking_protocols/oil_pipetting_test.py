dfa2c269febdb42128548ee5161dde07704b33e2
James-Kitson <jameskitson10@gmail.com>
2019-06-19 17:29:57 +0100

    testing labware loading unsuccessfully

--- Protocols/Labware_checking_protocols/oil_pipetting_test.py
+++ Protocols/Labware_checking_protocols/oil_pipetting_test.py
@@ -3,7 +3,7 @@ from opentrons import labware, instruments
 metadata = {
     'protocolName': 'Test multiplate',
     'author': 'James Kitson <James.Kitson@ncl.ac.uk>',
-    'description': 'A simple test protocol that checks I can pipatte into the same target well on multiple plates'
+    'description': 'A simple test protocol that checks I can pipette into the same target well on multiple plates'
     }
 
 # Set plates to use
@@ -14,7 +14,7 @@ PCR2 = labware.load('starlab-E1403-5200', '5')
 
 # set tips to use
 tips10 = labware.load('tiprack-starlab-S1181-3810', '2')
-tips300 = labware.load('opentrons-tiprack-300ul', '3')
+tips300 = labware.load('opentrons-tiprack-300ul', slot='3')
 
 # set pipettes
 pipette10 = instruments.P10_Multi(mount='right', tip_racks=[tips10])
@@ -27,11 +27,10 @@ dest_plates = [PCR1,PCR2]
 all_dests = [well for plate in dest_plates for well in plate.rows('A')]
 
 for d in all_dests:
-   # pipette300.pick_up_tip(tiprack_300['A'])
     pipette300.transfer(30, master_mix, d.top(), blow_out=True, new_tip='never')
 pipette300.drop_tip()
 
-    # forward primer distribution
+# forward primer distribution
 for ind, primer in enumerate(forward_primer.rows('A')):
     dests = [plate.rows('A')[ind] for plate in dest_plates]
     #pipette10.pick_up_tip(tiprack_10['A'])
