458fb38fd01cc7d32505e87907b716b4d4c88f2d
James-Kitson <jameskitson10@gmail.com>
2019-06-20 12:32:44 +0100

    got primer distribution working, still not recognising the opentrons tips in this protocol

--- Protocols/Labware_checking_protocols/multi_pipetting_test.py
+++ Protocols/Labware_checking_protocols/multi_pipetting_test.py
@@ -6,19 +6,17 @@ metadata = {
     'description': 'A simple test protocol that checks I can pipette into the same target well on multiple plates'
     }
 
-# Set plates to use
+# Set labware to use
 trough = labware.load('starlab-E2310-1200', '1')
-forward_primer = labware.load('starlab-E1403-0100','10')
+#tips300 = labware.load('opentrons-tiprack-300ul', '2')
+tips10 = labware.load('tiprack-starlab-S1181-3810', '3')
 PCR1 = labware.load('starlab-E1403-5200', '4')
 PCR2 = labware.load('starlab-E1403-5200', '5')
-
-# set tips to use
-tips10 = labware.load('tiprack-starlab-S1181-3810', '2')
-tips300 = labware.load('opentrons-tiprack-300ul', '3')
+forward_primer = labware.load('starlab-E1403-0100','10')
 
 # set pipettes
 pipette10 = instruments.P10_Multi(mount='right', tip_racks=[tips10])
-pipette300 = instruments.P300_Multi(mount='left', tip_racks=[tips300])
+#pipette300 = instruments.P300_Multi(mount='left', tip_racks=[tips300])
 
 master_mix = trough.wells('A3')
 
@@ -26,11 +24,13 @@ dest_plates = [PCR1,PCR2]
 
 all_dests = [well for plate in dest_plates for well in plate.rows('A')]
 
-for d in all_dests:
-    pipette300.transfer(30, master_mix, d.top(), blow_out=True, new_tip='never')
-pipette300.drop_tip()
+#for d in all_dests:
+#    pipette300.transfer(30, master_mix, d.top(), blow_out=True, new_tip='never')
+#pipette300.drop_tip()
 
 # forward primer distribution
 for ind, primer in enumerate(forward_primer.rows('A')):
     dests = [plate.rows('A')[ind] for plate in dest_plates]
-    pipette10.distribute(1, primer, dests)
+    pipette10.distribute(1, primer, dests,
+                         air_gap=5,
+                         trash=False)
