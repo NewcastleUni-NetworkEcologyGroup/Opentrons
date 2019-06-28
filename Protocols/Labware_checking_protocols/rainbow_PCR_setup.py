8d26dc97bad623ed506c390737690192fa4cc9a7
James-Kitson <James.Kitson@ncl.ac.uk>
2019-06-28 11:47:22 +0100

    adding some comments to explain my code better

--- Protocols/Labware_checking_protocols/rainbow_PCR_setup.py
+++ Protocols/Labware_checking_protocols/rainbow_PCR_setup.py
@@ -9,9 +9,9 @@ metadata = {
 # Set labware to use
 trough = labware.load('starlab-E2896-0220', '1', 'oil trough')
 trough2 = labware.load('starlab-E1403-0100', '7', 'mastermix trough')
-tips300 = labware.load('opentrons-tiprack-300ul', '5')
-tips10_1 = labware.load('opentrons-tiprack-10ul', '3')
-tips10_2 = labware.load('opentrons-tiprack-10ul', '6')
+tips300 = labware.load('opentrons-tiprack-300ul', '5', 'for oil and mastermix')
+tips10_1 = labware.load('opentrons-tiprack-10ul', '3', 'for primers')
+tips10_2 = labware.load('opentrons-tiprack-10ul', '6', 'for template DNA')
 PCR1 = labware.load('starlab-E1403-0100','4', 'output plate')
 forward_primer = labware.load('starlab-E1403-0100','10', 'primer plate')
 DNA = labware.load('starlab-E1403-0100','11', 'template DNA plate')
@@ -29,30 +29,30 @@ vol_transfer_primer: float = 2
 vol_transfer_DNA: float = 1
 
 # Define position of oil and mastermix
-#mineral_oil = tubes['A1']
-#mastermix = tubes['C1']
-
-# Define position of oil and mastermix
 mineral_oil = trough['A3']
 mastermix_red = trough2['A1']
 mastermix_yellow = trough2['A2']
 mastermix_blue = trough2['A3']
 
-# Define tube parameters
-total_length_5ml: float = 65.9
-length_barrel_5ml: float = 42.6
-tip_length_5ml: float = round(total_length_5ml-length_barrel_5ml,1)
-width_5ml: float = 15.3
+# =============================================================================
+# # Define tube parameters for 5ml source tubes
+# total_length_5ml: float = 65.9
+# length_barrel_5ml: float = 42.6
+# tip_length_5ml: float = round(total_length_5ml-length_barrel_5ml,1)
+# width_5ml: float = 15.3
+# =============================================================================
 
+#define deepwell plate parameters for 2.2ml deepwell plates
 total_length_deepwell: float = 37
 length_barrel_deepwell: float = 34
 tip_length_deepwell: float = round(total_length_deepwell-length_barrel_deepwell,1)
 width_deepwell: float = 8
 
-# This function will calculate the starting height for any pointy bottomed tube relative to top of the tube
-def start_height(start_vol, total_length, tip_type, tip_length, width):
-    #define the volume of the tip of the tube
-    if tip_type == 'round':
+# This function will calculate the starting height for any pointy bottomed tube relative to top of the tube.
+# This requires you to state whether the well is square or round as the tip volume will either be calculated as a cone or a pyramid.
+def start_height(start_vol, total_length, well_shape, tip_length, width):
+    #define the volume of the tip of the tube if the tip is a cone
+    if well_shape == 'round':
         vol_tip = 3.14*((width/2)**2)*(tip_length/3)
         # if the start volume > tip volume, work out the residual height, add it to the tip height and subrtact it from the total height of the tube
         if start_vol > vol_tip:
@@ -60,7 +60,8 @@ def start_height(start_vol, total_length, tip_type, tip_length, width):
         # if the starting volume is less than the tip volume, work out the total height and subrtact it from the total height of the tube
         else:
             return -(total_length - (3*(start_vol/(3.14*((width/2)**2)))))
-    elif tip_type == "pyramid":
+    #calulate the tip volume if the tip is a pyramid
+    elif well_shape == "square":
         vol_tip = (width*width*tip_length)/3
         # if the start volume > tip volume, work out the residual height, add it to the tip height and subrtact it from the total height of the tube
         if start_vol > vol_tip:
@@ -68,50 +69,51 @@ def start_height(start_vol, total_length, tip_type, tip_length, width):
         # if the starting volume is less than the tip volume, work out the total height and subrtact it from the total height of the tube
         else:
            return -(total_length - ((start_vol/((width*width*tip_length)/3))))
-    if tip_type != 'round' or 'pyramid':
-        raise Exception('tip_type must be either \'round\' or \'pyramid\'' )
+    if well_shape != 'round' or 'square':
+        raise Exception('well_shape must be either \'round\' or \'square\'' )
 
 
-# Calculate the distance from the top of the tube to the top of the mastermix at start
-#inverse_start_height_mastermix = round(start_height(start_vol = start_vol_mastermix,
-#                                                    total_length=total_length_5ml,
-#                                                    tip_type='round',
-#                                                    tip_length = tip_length_5ml,
-#                                                    width = width_5ml),1)
-# Calculate the distance from the top of the tube to the top of the oil at start
-#inverse_start_height_oil = round(start_height(start_vol = start_vol_oil,
-#                                              total_length=total_length_5ml,
-#                                              tip_type='round',
-#                                              tip_length = tip_length_5ml,
-#                                              width = width_5ml),1)
+# =============================================================================
+# # Calculate the distance from the top of the tube to the top of the mastermix at start
+# inverse_start_height_mastermix = round(start_height(start_vol = start_vol_mastermix,
+#                                                     total_length=total_length_5ml,
+#                                                     well_shape='round',
+#                                                     tip_length = tip_length_5ml,
+#                                                     width = width_5ml),1)
+# # Calculate the distance from the top of the tube to the top of the oil at start
+# inverse_start_height_oil = round(start_height(start_vol = start_vol_oil,
+#                                               total_length=total_length_5ml,
+#                                               well_shape='round',
+#                                               tip_length = tip_length_5ml,
+#                                               width = width_5ml),1)
+# =============================================================================
 
 # Calculate the distance from the top of the tube to the top of the mastermix at start
 inverse_start_height_mastermix = round(start_height(start_vol = start_vol_mastermix,
                                                     total_length=total_length_deepwell,
-                                                    tip_type='pyramid',
+                                                    well_shape='square',
                                                     tip_length = tip_length_deepwell,
                                                     width = width_deepwell),1)
 # Calculate the distance from the top of the tube to the top of the oil at start
 inverse_start_height_oil = round(start_height(start_vol = start_vol_oil,
                                               total_length=total_length_deepwell,
-                                              tip_type='pyramid',
+                                              well_shape='square',
                                               tip_length = tip_length_deepwell,
                                               width = width_deepwell),1)
 
-
+# The following function creates a tracking value for the liquid height in a well
 def height_track(transfer_vol):
     global remain_vol
     remain_vol = remain_vol-transfer_vol # should this be 8*volume_of_mineral_oil_in_ul for a trough?
     remain_height = round(start_height(start_vol = remain_vol,
                                               total_length=total_length_deepwell,
-                                              tip_type='pyramid',
+                                              well_shape='square',
                                               tip_length = tip_length_deepwell,
                                               width = width_deepwell),1)
     return remain_height
 
-
+# Before starting set the remaining vol of oil to the starting volume
 remain_vol = start_vol_oil
-#pipette_height_oil = height_track(vol_transfer_oil)
 
 # transfer mineral oil
 pipette300.set_flow_rate(aspirate=5, dispense=5)
@@ -127,7 +129,6 @@ for dest_col in range(12):
     pipette300.move_to(PCR1.cols(dest_col).top(-1)) 
     pipette300.delay(seconds=2)
     pipette300.touch_tip(radius = 0.8)
-    #t_count += 1
 pipette300.drop_tip()
 
 # set a source desitination with negatives in and a fancy pattern of destination wells
