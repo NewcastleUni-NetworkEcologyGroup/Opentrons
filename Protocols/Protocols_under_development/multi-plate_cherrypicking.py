361c1daf362ff1c89c46f5deb2cd2ab3e1fc5f78
James-Kitson <James.Kitson@ncl.ac.uk>
2019-08-02 11:13:55 +0100

    tidies up the code a fair bit with extra annotation

--- Protocols/Protocols_under_development/multi-plate_cherrypicking.py
+++ Protocols/Protocols_under_development/multi-plate_cherrypicking.py
@@ -15,22 +15,27 @@ metadata = {
 # 
 # =============================================================================
 
+# File path on robot
 #input_file_path = '/data/cherrypicking_csvs/cherrypicking.csv'
-input_file_path = '/Users/jameskitson/Documents/work/Opentrons/Protocols/Protocols_under_development/cherrypicking.csv'
+# File path on local machine for simulation
+input_file_path = '/home/neg/Documents/Projects/Cratopus/Cratopus-chrysochlorus-diet/cherrypicking.csv'
 volumes_csv = open(input_file_path, 'r').read()
+
+# Set up other parameters for the code to follow
 pipette_model = 'p50'
 source_plate_type = 'starlab-E1403-0100'
 destination_plate_type = 'starlab-E1403-0100'
 tip_reuse = 'new tip each time'
 tip_type = 'tiprack-starlab-S1120-2810'
-number_of_source_plates = 2
 
+# Set a max volume of pipette based on the selcted model to automatically load the correct pipette - can probably simplify this a lot
 pipette_max_vol = int(pipette_model[1:])
 
+# Assign slots for tips and destination plate
 tiprack_slots = ['10']
 dest_plate = labware.load(destination_plate_type, '11')
 
-# set pipette type based on maximum volume
+# Set pipette type based on maximum volume
 if pipette_max_vol == 300:
     tipracks = [
         labware.load(tip_type, slot) for slot in tiprack_slots]
@@ -48,27 +53,27 @@ elif pipette_max_vol == 1000:
         labware.load(tip_type, slot) for slot in tiprack_slots]
     pipette = instruments.P1000_Single(mount='left', tip_racks=tipracks)
 
-# sort the raw data into a list
+# Sort the raw data into a list
 data = [
     [slot, source_plate, source_well, vol]
     for slot, source_plate, source_well, vol in
     [row.split(',') for row in volumes_csv.strip().splitlines() if row]
 ]
 
-# create a list to keep track of slots we've already used, incuding slots preallocated to tips and output plate
+# Create a list to keep track of slots we've already used, incuding slots preallocated to tips and output plate
 slot_list=['10','11']
 
 for idx, (slot, source_plate, source_well, vol) in enumerate(data):
-    # check we've not already filled the slot
+    # Check we've not already filled the slot
     if slot not in slot_list:
-        #create the labware and add the slot to slot_list
+        # Create the labware and add the slot to slot_list
         vars()[source_plate]=labware.load(source_plate_type, str(slot), 'source plate')
         slot_list.append(slot)
 
-# set how you want to use tips
+# Set how you want to use tips
 tip_strategy = 'always' if tip_reuse == 'new tip each time' else 'once'
 
-# make the transfers using the nested values in the data list
+# Make the transfers using the nested values in the data list
 for well_idx, (slot, source_plate, source_well, vol) in enumerate(data):
     vol = float(vol)
     pipette.transfer(
