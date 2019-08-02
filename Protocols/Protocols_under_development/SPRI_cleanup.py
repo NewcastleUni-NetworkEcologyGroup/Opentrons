79f88177973abeca235f12d11745234d416b4c25
James-Kitson <James.Kitson@ncl.ac.uk>
2019-08-02 15:59:27 +0100

    addedd a starting point for an SPRI protocol

---
+++ Protocols/Protocols_under_development/SPRI_cleanup.py
@@ -0,0 +1,19 @@
+from opentrons import labware, instruments, modules
+
+metadata = {
+    'protocolName': 'SPRI bead cleanups',
+    'author': 'James Kitson',
+    }
+
+magdeck = modules.load('magdeck', '7') 
+tips50 = labware.load('tiprack-starlab-S1120-2810', '1')
+pipette = instruments.P50_Single(mount='left',tip_racks=[tips50])
+plate = labware.load('starlab-E1403-5200', '7', share = True) 
+
+magdeck.engage(height=10)
+pipette.pick_up_tip()
+pipette.move_to(plate.wells('A1','D5','H11'))
+pipette.drop_tip(tips50['A1'])
+pipette.delay(10)
+
+magdeck.disengage()
