2ee980e4d00dfccc14a327c4894098e16c4608a6
James-Kitson <jameskitson10@gmail.com>
2019-08-06 17:57:51 +0100

    just making sure that the latest versions of the SPRI protocol are on github

--- Protocols/Protocols_under_development/SPRI_cleanup.py
+++ Protocols/Protocols_under_development/SPRI_cleanup.py
@@ -10,10 +10,22 @@ tips50 = labware.load('tiprack-starlab-S1120-2810', '1')
 pipette = instruments.P50_Single(mount='left',tip_racks=[tips50])
 plate = labware.load('starlab-E1403-5200', '7', share = True) 
 
-magdeck.engage(height=10)
 pipette.pick_up_tip()
-pipette.move_to(plate.wells('A1'))
 pipette.drop_tip(tips50['A1'])
-pipette.delay(10)
 
+
+magdeck.engage(height=15)
+pipette.delay(5)
+magdeck.disengage()
+
+magdeck.engage(height=16)
+pipette.delay(5)
+magdeck.disengage()
+
+magdeck.engage(height=17)
+pipette.delay(10)
+magdeck.disengage()
+
+magdeck.engage(height=18)
+pipette.delay(10)
 magdeck.disengage()
