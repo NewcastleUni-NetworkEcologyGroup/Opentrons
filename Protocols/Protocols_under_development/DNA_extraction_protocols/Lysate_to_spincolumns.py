d2b92b64813ec9f8475bbbe7c757ce8a49e9ce67
James-Kitson <James.Kitson@ncl.ac.uk>
2019-08-16 10:55:35 +0100

    had the wrong sort of plates in! Fixed now

--- Protocols/Protocols_under_development/DNA_extraction_protocols/Lysate_to_spincolumns.py
+++ Protocols/Protocols_under_development/DNA_extraction_protocols/Lysate_to_spincolumns.py
@@ -7,11 +7,11 @@ metadata = {
     }
 
 # get the plates loaded in the middle of the deck so we can do fancy multichannel pickups
-lysate_plate1 = labware.load('starlab-E1403-0100', '4', 'input lysate plate')
-lysate_plate2 = labware.load('starlab-E1403-0100', '5', 'input lysate plate')
+lysate_plate1 = labware.load('starlab-E2896-0220', '4', 'input lysate plate')
+lysate_plate2 = labware.load('starlab-E2896-0220', '5', 'input lysate plate')
 
-mix_plate1 = labware.load('starlab-E1403-0100', '7', 'mixing plate')
-mix_plate2 = labware.load('starlab-E1403-0100', '8', 'mixing plate')
+mix_plate1 = labware.load('starlab-E2896-0220', '7', 'mixing plate')
+mix_plate2 = labware.load('starlab-E2896-0220', '8', 'mixing plate')
 
 spin_cols1 = labware.load('spin_cols_96', '10', 'mixing plate')
 spin_cols2 = labware.load('spin_cols_96', '11', 'mixing plate')
@@ -64,7 +64,7 @@ for idx, plate in enumerate(lysates):
                             mixes[idx][well].bottom(0.5),
                             spins[idx][well].top(-4),
                             air_gap=20,
-                            touch_tip=True,
+                            touch_tip=False,
                             new_tip='never').blow_out(spins[idx][well].top(-4))
         pipette300.drop_tip()
         
