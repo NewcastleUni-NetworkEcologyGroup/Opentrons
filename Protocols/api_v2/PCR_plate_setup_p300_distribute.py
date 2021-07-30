95b2647b3a4e2ee8698a4b1607553608383439ec
James-Kitson <jameskitson10@gmail.com>
2021-07-30 16:53:07 +0100

    moved final mastermix aspirate up 0.1mm to prevent tip sealing

--- Protocols/api_v2/PCR_plate_setup_p300_distribute.py
+++ Protocols/api_v2/PCR_plate_setup_p300_distribute.py
@@ -105,7 +105,7 @@ def run(protocol: protocol_api.ProtocolContext):
                                 blow_out=True,
                                 blow_out_location='source well',
                                 disposal_volume=2)
-        pipette_300.well_bottom_clearance.aspirate = round(initial_mastermix_height-((initial_mastermix_height/steps)*(ind+2)),1)+0.2
+        pipette_300.well_bottom_clearance.aspirate = round(initial_mastermix_height-((initial_mastermix_height/steps)*(ind+2)),1)+0.3
         pipette_300.drop_tip()        
 
     # forward primer distribution
