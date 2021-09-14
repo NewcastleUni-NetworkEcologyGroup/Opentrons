1ff76fc602b3d4a2d1ba1233afcce701165b309a
James-Kitson <jameskitson10@gmail.com>
2021-09-14 14:43:47 +0100

    a few more changes to fix minor issues with pipette heights

--- Protocols/api_v2/PCR_plate_oil_distribute.py
+++ Protocols/api_v2/PCR_plate_oil_distribute.py
@@ -9,16 +9,15 @@ from opentrons import protocol_api
 
 # metadata
 metadata = {'apiLevel': '2.8',
-            'protocolName': 'multiPCR plate setup',
+            'protocolName': 'Oil multiplate dispense',
             'author': 'James Kitson <james.kitson@newcastle.ac.uk>',
-            'description': 'A protocol to distribute PCR master mix and tagged primers to multiple PCR plaes'}
+            'description': 'A protocol to distribute mineral oil to multiple PCR plates'}
 
 # protocol run function. the part after the colon lets your editor know
 # where to look for autocomplete suggestions
 
 def run(protocol: protocol_api.ProtocolContext):
-   
-   
+      
     # key labware dimensions
     tip_height = 3
     well_width =8.2
@@ -26,12 +25,14 @@ def run(protocol: protocol_api.ProtocolContext):
     
     # check for labware space
     available_slots = [1,2,4,5,6,7,8,10,11,12] # this order minimises pipette travel over non-target wells
-    number_of_destination_plates: int = 9
+    number_of_destination_plates: int = 1
     max_destination_plates = 9
     if number_of_destination_plates > max_destination_plates:
         raise Exception('Please specify 9 or fewer destination plates, you cannot hold enough oil in this reservoir')
+   
     # key liquid volumes
     oil_vol = 15
+    # set a fudge factor that is more generous for smaller numbers of plates
     fudge_factor = round(1.2-(0.1/(max_destination_plates/number_of_destination_plates)),2)
     
     # work out the initial master mix volume
@@ -78,42 +79,28 @@ def run(protocol: protocol_api.ProtocolContext):
     pipette_300.flow_rate.aspirate = 10
     initial_oil_height = start_height(start_vol, tip_height, well_width, well_length)
     print("The fudge factor for oil volumes is: ", fudge_factor)
-    print("The initial oil volume is: ", end='')
-    print(start_vol)
-    print("The initial oil height is: ", end='')
-    print(initial_oil_height)
+    print("The initial oil volume is: ", start_vol)
+    print("The initial oil height is: ", initial_oil_height)
     
-    # Define initial pipetting height that deals with workng on single plates
+    # Define initial pipetting height that deals with workng on single rows
     if round(initial_oil_height-(initial_oil_height/steps),1) > 0:
         pipette_300.well_bottom_clearance.aspirate = round(initial_oil_height-(initial_oil_height/steps),1)
     elif round(initial_oil_height-(initial_oil_height/steps),1) == 0:
         pipette_300.well_bottom_clearance.aspirate = 0.3
+    print("The initial oil pipetting height is: ", pipette_300.well_bottom_clearance.aspirate)
     
     # # Set some really slow pipetting rates for the viscous oil    
     pipette_300.flow_rate.dispense = 10
     pipette_300.well_bottom_clearance.dispense = 5
     pipette_300.flow_rate.blow_out = 10
 
-    # # distribute oil using distribute command and well referencing - may need replacing with aspirate and dispense if I need waiting times etc
-    # for ind, d in enumerate(dest_plates):
-    #     pipette_300.pick_up_tip()
-    #     print('Round', ind+1, 'pipetting height', pipette_300.well_bottom_clearance.aspirate)
-    #     pipette_300.distribute(oil_vol,oil,d.wells(),
-    #                             touch_tip=True,
-    #                             radius=0.8,
-    #                             new_tip='never',
-    #                             blow_out=True,
-    #                             blow_out_location='source well',
-    #                             disposal_volume=30)
-    #     pipette_300.well_bottom_clearance.aspirate = round(initial_oil_height-((initial_oil_height/steps)*(ind+2)),1)+0.3
-    #     pipette_300.drop_tip()                
-        
-    ticker = 0        
+    # Start a ticker to track pipetting operations
+    ticker = 1
+    
     # distribute oil using distribute command and well referencing - may need replacing with aspirate and dispense if I need waiting times etc
     for d in dest_plates:
         pipette_300.pick_up_tip()
         for target_well in d.rows_by_name()['A']:
-          ticker = ticker+1
           print('Round', ticker, 'pipetting height', pipette_300.well_bottom_clearance.aspirate)
           # move to oil reservoir and aspirate then wait for viscous fluid to catch up
           pipette_300.aspirate(oil_vol,oil)
@@ -129,7 +116,15 @@ def run(protocol: protocol_api.ProtocolContext):
           pipette_300.move_to(target_well.top(-2))
           protocol.delay(seconds=2)
           pipette_300.touch_tip(target_well, radius=0.75,v_offset=-2)
-          # change the aspirate height on the oil to move down a bit
-          pipette_300.well_bottom_clearance.aspirate = round(initial_oil_height-((initial_oil_height/steps)*(ticker+1)),1)+0.3
+          
+          # tasks done so add one to the ticker to calulate new pipette heights.
+          ticker = ticker+1
+                    
+          # change the aspirate height on the oil to move down a bit, if it is the last step, set a special heigh that isn't zero and slow the aspirate right down
+          if ticker < steps:
+              pipette_300.well_bottom_clearance.aspirate = round((initial_oil_height)-((initial_oil_height/steps)*(ticker)),1)
+          elif ticker == steps:
+              pipette_300.well_bottom_clearance.aspirate = 0.3
+              pipette_300.flow_rate.aspirate = 5
         pipette_300.drop_tip()                
             
