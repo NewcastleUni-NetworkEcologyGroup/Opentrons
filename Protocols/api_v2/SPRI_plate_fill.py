63fe40140a11b414a4f2e567769ba85ca0b5ee75
James-Kitson <james.kitson@newcastle.ac.uk>
2021-09-22 10:49:59 +0100

    more changes to liquid handling for multiple plate operations

--- Protocols/api_v2/SPRI_plate_fill.py
+++ Protocols/api_v2/SPRI_plate_fill.py
@@ -23,20 +23,20 @@ def run(protocol: protocol_api.ProtocolContext):
     
     # check for labware space
     available_slots = [1,2,3,4,6,7,8,9]
-    number_of_destination_plates: int = 2
+    number_of_destination_plates: int = 1
     if number_of_destination_plates > 5:
         raise Exception('Please specify 5 or fewer destination plates, you cant hold enough SPRI beads in a 2.2ml plate for more.') 
         
      #### Step 6 - pause, cover and shake ####
     protocol.pause(
-            "Have you put 2000 ul of SPRI beads in column one of the reservoir?")
+            "Have you put 400 ul of SPRI beads per plate in each well column one of the reservoir?")
     
 
     # labware for protocol
     reservoir = protocol.load_labware('sarstedt_96_wellplate_2200ul',5)
     beads = reservoir['A1']
     # start_vol_beads = 2000
-    tiprack_300 = protocol.load_labware('opentrons_96_tiprack_300ul', 10)
+    tiprack_200 = protocol.load_labware('opentrons_96_filtertiprack_200ul', 10)
     spri_plate_name = 'sarstedt_96_skirted_wellplate_200ul'
     dest_plates = [protocol.load_labware(spri_plate_name, str(slot))
                for slot in [available_slots[i] for i in range(number_of_destination_plates)]]
@@ -48,7 +48,7 @@ def run(protocol: protocol_api.ProtocolContext):
     # width_deepwell: float = 8.3
     
     # pipettes
-    left_pipette = protocol.load_instrument('p300_multi', mount='right', tip_racks=[tiprack_300]) 
+    left_pipette = protocol.load_instrument('p300_multi', mount='left', tip_racks=[tiprack_200]) 
     
     #generate a list of detinations to target for the distribute command
     #all_dests = [well for plate in dest_plates for well in plate.rows('A')]
