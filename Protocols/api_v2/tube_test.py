99c9b5c44c88393ecf8a2d4f7f139203e6b1af4c
James-Kitson <james.kitson@newcastle.ac.uk>
2021-10-06 16:19:07 +0100

    adding a quick protocol to test the tube rack

--- Protocols/api_v2/tube_test.py
+++ Protocols/api_v2/tube_test.py
@@ -21,9 +21,11 @@ def run(protocol: protocol_api.ProtocolContext):
     # set up the reagent locations
     DNA_plate = protocol.load_labware('sarstedt_96_unskirted_wellplate_200ul_on_coldblock',5,'Source DNA plate')
     PCR_plate = protocol.load_labware('sarstedt_96_skirted_wellplate_200ul',8,'Target PCR plate')
+    out_tube = protocol.load_labware('opentrons_24_tuberack_generic_2ml_screwcap',7)
     
     # set up tip locations
     tiprack_20 = protocol.load_labware('opentrons_96_filtertiprack_20ul', 2)
+    tiprack_controls = protocol.load_labware('opentrons_96_filtertiprack_20ul', 6)
     
     # set up pipettes
     pipette_10 = protocol.load_instrument('p10_multi', mount='right', tip_racks=[tiprack_20]) 
@@ -40,5 +42,17 @@ def run(protocol: protocol_api.ProtocolContext):
                     disposal_volume=0.5,
                     new_tip='always')
 
- 
-          
+    
+    pipette_10.transfer(2,
+                    out_tube.wells_by_name('A1'),
+                    PCR_plate.wells_by_name('C4'),
+                    mix_before=(5,5),
+                    disposal_volume=0.5,
+                    new_tip='always')
+
+    pipette_10.transfer(2,
+                    out_tube.wells_by_name('A1'),
+                    PCR_plate.wells_by_name('D5'),
+                    mix_before=(5,5),
+                    disposal_volume=0.5,
+                    new_tip='always')
